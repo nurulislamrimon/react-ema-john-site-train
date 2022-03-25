bd68fe89723924ffb11775bd7f4d7b21769f4419
nurulislamrimon <nurulislamrimon@gmail.com>
2022-03-25 19:51:36 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import GetProducts from './components/GetProducts/GetProducts';
 import RightSection from './components/RightSection/RightSection';
+import { addToDb, getProductFromDb } from "../src/utilities/fakedb";
 
 
 function App() {
+  // get data from fake db
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('products.json').then(res => res.json()).then(data => setProducts(data))
+  }, [])
+
+  // cart item for dispalying
   const [cart, setCart] = useState([]);
 
-  const CartItem = product => {
-    setCart([...cart, product])
+  const CartItem = selectedProduct => {
+
+    let newCart = [];
+
+    const exist = cart.find(product => product.id === selectedProduct.id);
+
+    if (!exist) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter(product => product.id !== selectedProduct.id);
+      exist.quantity = exist.quantity + 1;
+      newCart = [...rest, exist];
+    }
+
+    setCart([...cart, selectedProduct]);
+    addToDb(selectedProduct.id)
   };
+  useEffect(() => {
+    const cartedProducts = getProductFromDb();
+    const savedCart = [];
+    for (const id in cartedProducts) {
+      const addedProducts = products.find(product => product.id === id);
+      if (addedProducts) {
+        const quantity = cartedProducts[id];
+        addedProducts.quantity = quantity;
+        savedCart.push(addedProducts)
+      }
+      setCart(savedCart)
+      console.log(savedCart);
+    };
+  }, [products])
 
   return (
     <div className='main-container'>
--- src/components/RightSection/RightSection.js
+++ src/components/RightSection/RightSection.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const RightSection = props => {
+const RightSection = ({ Cart }) => {
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const item of Cart) {
+        quantity = item.quantity + quantity;
+        total = total + item.price * quantity;
+        shipping = shipping + item.shipping;
+    }
+    const tax = (total * 10 / 100).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
     return (
-        <div>
-            <h1>Selected items: {(props.Cart.length)}</h1>
+        <div className='right-section'>
+            <div className="carted-items">
+                <h1>Cart:</h1>
+                <h4>Selected items: {(quantity)}</h4>
+                <h4>Total Price: ${(total)}</h4>
+                <h4>Shipping Cost: ${(shipping)}</h4>
+                <h4>Tax: ${(tax)}</h4>
+                <h2>Grand total: ${(grandTotal)}</h2>
+            </div>
         </div>
     );
 };
